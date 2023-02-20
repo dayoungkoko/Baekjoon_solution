0a86e716f76bf5d7f8e16bf039b732986868024f
dayoungkoko <116150072+dayoungkoko@users.noreply.github.com>
2023-02-20 22:46:44 +0900

    2798번 블랙잭

---
+++ Brute Force/2798.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+
+def blackjack(num, limit) :
+    result = []
+    for i in range(num-2) :
+        for j in range(i+1, num-1) :
+            for k in range(j+1, num) :
+                if lst[i]+lst[j]+lst[k] <= limit :
+                    result.append(lst[i] + lst[j] + lst[k])
+    return max(result)
+        
+answer = blackjack(n, m)
+print(answer)
